c25603b036b6ac10b5a86a15e7044ef2bf685234
ashish@bcaster.com <ashish@bcaster.com>
2019-11-25 19:24:47 +0200

    Updates

---
+++ precache-manifest.6d34116c166cffce09b1ce650621d012.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d0882e76ca67697d6d2ef4498345b32",
+    "url": "/reactCrud/index.html"
+  },
+  {
+    "revision": "d8c8f25d2523c95948b2",
+    "url": "/reactCrud/static/css/main.3e3b6a98.chunk.css"
+  },
+  {
+    "revision": "fcddf51088d84bc8bd3f",
+    "url": "/reactCrud/static/js/2.f8f36956.chunk.js"
+  },
+  {
+    "revision": "d8c8f25d2523c95948b2",
+    "url": "/reactCrud/static/js/main.5fb650c1.chunk.js"
+  },
+  {
+    "revision": "20a5a2b5f6c635f2ca52",
+    "url": "/reactCrud/static/js/runtime-main.034326bf.js"
+  }
+]);
